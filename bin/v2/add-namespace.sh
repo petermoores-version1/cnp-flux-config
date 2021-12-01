79b528bbed6c791cf2db418858f2ec0976ad345a
Nick Moynihan <90863237+nickmoyni@users.noreply.github.com>
2021-12-01 12:39:05 +0000

    Revert "fixing v2/add-namespace.sh script"

--- bin/v2/add-namespace.sh
+++ bin/v2/add-namespace.sh
@@ -33,13 +33,13 @@ EOF
 
 (
 cat <<EOF
-apiVersion: kustomize.toolkit.fluxcd.io/v1beta2
+apiVersion: kustomize.toolkit.fluxcd.io/v1beta1
 kind: Kustomization
 metadata:
   name: $NAMESPACE
   namespace: flux-system
 spec:
-  path: ./apps/$NAMESPACE/\${ENVIRONMENT}/base
+  path: ./apps/$NAMESPACE/\${ENVIRONMENT}
   postBuild:
     substitute:
       NAMESPACE: "$NAMESPACE"
